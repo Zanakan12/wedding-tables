f3dd0b45d2d947a33a9f54edb1598b8b6a3fa8a8
Zanakan12 <zanakan12maniac@proton.me>
2025-05-27 16:59:42 +0200

    change marge of table

--- src/data/tables.ts
+++ src/data/tables.ts
@@ -4,6 +4,6 @@ import { Table } from '@/types';
 export const tables: Table[] = Array.from({ length: 20 }, (_, i) => ({
   id: i + 1,
   name: `Table${i + 1}`,
-  x: (i % 5) * 100,     // 5 tables par ligne
-  y: Math.floor(i / 5) * 100, // 4 lignes
+  x: (i % 5) * 70,     // 5 tables par ligne
+  y: Math.floor(i / 5) * 70, // 4 lignes
 }));
